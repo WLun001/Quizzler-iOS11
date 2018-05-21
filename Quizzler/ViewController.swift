d9f1b7e6255520cbd3df88e5ade8cd3a20c0cda2
Wei Lun <weilun_95@hotmail.com>
2018-05-21 15:17:58 +0800

    added basic function

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let BUTTON_YES = 1
+    let BUTTON_NO =  2
+    let MAX_ANS = 13
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank().list
+    var questionCount = 0
+    var score = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,32 +24,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        checkAnswer(allQuestions[questionCount], sender.tag)
+        nextQuestion()
+        updateUI()
     }
     
     
     func updateUI() {
-      
+        questionLabel.text = (allQuestions[questionCount] as Question).questionText
+        scoreLabel.text = String(score)
+        progressLabel.text = "\(questionCount + 1)/\(MAX_ANS)"
     }
     
 
     func nextQuestion() {
-        
+        if  questionCount < MAX_ANS - 1{
+            questionCount += 1
+        }
     }
     
     
-    func checkAnswer() {
-        
+    func checkAnswer(_ ques: Question, _ response: Int) {
+        if convertToBool(response) == ques.answer {
+            score += 1
+        }
     }
     
     
     func startOver() {
        
+    }
+    
+    func convertToBool(_ i: Int) -> Bool {
+        return i == BUTTON_YES ? true : false
     }
     
 
